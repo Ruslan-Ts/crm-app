056e9a1af178fe5d747734ead43451f38c2ec292
Ruslan Tsuman <r.ts@me.com>
2024-07-14 15:48:37 +0200

    button,input

---
+++ src/app/components/input-field.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export interface InputFieldProps {
+  label?: string;
+}
+
+export default function InputField({ label, id, ...rest }: InputFieldProps) {
+  return (
+   <div className='flex flex-col'>{label && (
+   <label htmlFor={id} className='mb-2 text-base color-gray-900'>{label}</label>
+)}
+</div>
+  );
+}
